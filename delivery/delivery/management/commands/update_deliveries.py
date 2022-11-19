d8dfb420c6bbd0601d645e7323117643b55de6d9
Terry McDonnell <terry.mcdonnell@gmail.com>
2022-11-19 12:57:27 -0800

    delivery update script rev

--- delivery/delivery/management/commands/update_deliveries.py
+++ delivery/delivery/management/commands/update_deliveries.py
@@ -7,12 +7,13 @@ class Command(BaseCommand):
     help = "Run code against all existing delivery orders"
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument("--id", type=int)
 
     def handle(self, *args, **options):
-        deliveries = Delivery.objects.all()
+        if options["id"]:
+            deliveries = [Delivery.objects.get(pk=options["id"])]
+        else:
+            deliveries = Delivery.objects.all()
         for delivery in deliveries:
-            if delivery.id != 146:
-                continue
             delivery.sync()
             delivery.save()
